a5bc1f72c5e40cb640ed93adaf99af7db7ef7a14
Blora616 <Blora616@gmail.com>
2019-06-19 19:50:54 -0400

    3d assets

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { compose } from 'recompose';
-import { connect } from 'react-redux';
-import themes, { overrides } from './themes';
-import { withLayout } from "./components/UI/Layout"
-import Layout from './components/DashboardPage/Layout';
-import Error from './pages/Error';
-import Login from './pages/Login';
-import Home from "./pages/Home"
-import Checkout from './pages/Checkout';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { compose } from "recompose";
+import { connect } from "react-redux";
+import { withLayout } from "./components/UI/Layout";
+import themes, { overrides } from "./themes";
+import Layout from "./components/DashboardPage/Layout";
+import Error from "./pages/Error";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import EnkeepThree from "./components/3DCanvas";
 
-const theme = createMuiTheme({...themes.default, ...overrides});
+const theme = createMuiTheme({ ...themes.default, ...overrides });
 
 const PrivateRoute = ({ component, ...rest }) => {
   return (
     <Route
-      {...rest} render={props => (
-      localStorage.getItem('token') ? (
-        React.createElement(component, props)
-      ) : (
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: { from: props.location },
-          }}
-        />
-      )
-    )}
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          React.createElement(component, props)
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
     />
   );
 };
@@ -35,17 +37,18 @@ const PrivateRoute = ({ component, ...rest }) => {
 const PublicRoute = ({ component, ...rest }) => {
   return (
     <Route
-      {...rest} render={props => (
-      localStorage.getItem('token') ? (
-        <Redirect
-          to={{
-            pathname: '/admin',
-          }}
-        />
-      ) : (
-        React.createElement(component, props)
-      )
-    )}
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Redirect
+            to={{
+              pathname: "/admin"
+            }}
+          />
+        ) : (
+          React.createElement(component, props)
+        )
+      }
     />
   );
 };
@@ -56,20 +59,22 @@ const App = () => (
       <Switch>
         <Route exact path="/" render={() => withLayout(Home)} />
         <Route exact path="/checkout" render={() => withLayout(Checkout)} />
-        <Route exact path="/admin" render={() => <Redirect to="/admin/dashboard" />} />
+        <Route
+          exact
+          path="/admin"
+          render={() => <Redirect to="/admin/dashboard" />}
+        />
         <PrivateRoute path="/admin" component={Layout} />
         <PublicRoute path="/login" component={Login} />
+        <Route path="/enkeep" component={EnkeepThree} />
         <Route component={Error} />
       </Switch>
     </BrowserRouter>
   </MuiThemeProvider>
 );
 
-
 export default compose(
-  connect(
-    state => ({
-      isAuthenticated: state.login.isAuthenticated,
-    })
-  )
+  connect(state => ({
+    isAuthenticated: state.login.isAuthenticated
+  }))
 )(App);
---
+++ src/components/3DCanvas/index.js
@@ -0,0 +1,55 @@
+import React, { useRef, useMemo, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Canvas, extend, useThree, useRender } from "react-three-fiber";
+import "./styles.css"
+
+const path = 'https://raw.githubusercontent.com/Octothorp6/3d-Model/master/EtherNode_Final/Final_2.gltf'
+
+extend({ OrbitControls });
+
+function Model({ url }) {
+  const [gltf, set] = useState();
+  useMemo(
+    () =>
+      new GLTFLoader().load(
+        url,
+        set
+      ),
+      [url]
+  );
+  return  gltf ? <primitive object={gltf.scene} /> : null;
+}
+
+function Controls(props) {
+  const { camera, canvas } = useThree();
+  const controls = useRef();
+  useRender(() => controls.current && controls.current.update());
+  
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, canvas.parentNode]}
+      {...props}
+    />
+  );
+}
+
+export default function EnkeepThree() {
+  return (
+    <>
+      <Canvas>
+        <Controls
+          enableDamping
+          enablePan={false}
+          dampingFactor={0.1}
+          rotateSpeed={0.1}
+          maxPolarAngle={Math.PI / 2}
+        />
+        <ambientLight intensity={0.9} position={[300, 300, 400]} />
+        <spotLight intensity={0.8} position={[300,300,400]} />
+        <Model url={path} />
+      </Canvas>
+    </>
+  );
+}
