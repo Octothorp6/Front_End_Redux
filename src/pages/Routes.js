5b47aeeeb1330c11b3734f810e4513a2a3f3091b
Blora616 <Blora616@gmail.com>
2019-06-21 19:40:29 -0400

    fix bundle

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,23 +1,29 @@
-import React, { Suspense } from "react";
+import React from "react";
+import loadable from "@loadable/component";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Error from "./Error";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import { withLayout } from "../components/UI/Layout";
 
-const LazyLogin = React.lazy(() => import("./Login"));
+//LAZY LOADING ROUTES ONLY AS THEY ARE NEEDED
+const LazyLogin = loadable(() => 
+  import("./Login")
+);
 
-const LazyAdmin = React.lazy(() =>
+const LazyAdmin = loadable(() =>
   import("../components/DashboardPage/Layout")
 );
 
-const LazyEnkeep = React.lazy(() => import("../components/3DCanvas/index"));
+const LazyEnkeep = loadable(() => 
+  import("../components/3DCanvas/index")
+);
 
 export default function asyncComponent(Component) {
   return props => (
-    <Suspense fallback={<Home />}>
+    <React.Fragment>
       <Component {...props} />
-    </Suspense>
+    </React.Fragment>
   );
 }
 
