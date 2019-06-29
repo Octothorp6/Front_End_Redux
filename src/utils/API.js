7636346228ff13e29a1b04deb5da51ac54ef1148
Blora616 <Blora616@gmail.com>
2019-06-29 13:00:33 -0400

    API helperas

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,59 +1,54 @@
 import axios from "axios";
 
+const baseUrl = process.env.REACT_APP_ENCOMMERCE_API;
+
+//POST REQUEST HELPERS
+function postWithToken(suffix, data, token) {
+  return axios.post(baseUrl + suffix, data, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+}
+
+function postWithoutToken(suffix, data) {
+  return axios.post(baseUrl + suffix, data);
+}
+// GET REQUEST HELPERS
+function getWithToken(suffix, token) {
+  return axios.get(baseUrl + suffix, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+}
+
+function getWithoutToken(suffix, data) {
+  return axios.get(baseUrl + suffix, data);
+}
+
 export default {
-  // ROUTES WITHOUT TOKEN
+  // POST REQUESTS WITHOUT TOKEN
   sendEmail: function(email) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/mailRPC/mailRPC",
-      email
-    );
+    return postWithoutToken("/mailRPC/mailRPC", email);
   },
   register: function(user) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/accountRPC/acc",
-      user
-    );
+    return postWithoutToken("/accountRPC/acc", user);
   },
   login: function(user) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/accountRPC/acc",
-      user
-    );
+    return postWithoutToken("/accountRPC/acc", user);
   },
-  // ROUTES WITH TOKEN
+  // POST REQUESTS WITH TOKEN
   sendPreorderEmail: function(data, token) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/mailRPC/mailRPC",
-      data,
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
+    return postWithToken("/mailRPC/mailRPC", data, token);
   },
   newOrder: function(orderData, token) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/authRPC/authnet",
-      orderData,
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
+    return postWithToken("/authRPC/authnet", orderData, token);
   },
   newInvoice: function(query, token) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/btcpayRPC/btcpayRPC",
-      query,
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
+    return postWithToken("/btcpayRPC/btcpayRPC", query, token);
   },
   newTransaction: function(txData, token) {
-    return axios.post(
-      process.env.REACT_APP_ENCOMMERCE_API + "/orderRPC/orderRPC",
-      txData,
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
+    return postWithToken("/orderRPC/orderRPC", txData, token);
   },
+  // GET REQUESTS WITH TOKEN
   getUsers: function(token) {
-    return axios.get(
-      process.env.REACT_APP_ENCOMMERCE_API +
-        "/accountRPC/admin" +
-        { headers: { Authorization: `Bearer ${token}` } }
-    );
+    return getWithToken("/accountRPC/admin", token);
   }
 };
